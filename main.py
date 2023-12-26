1b575095feb8c548c6a9004e526bbace40037905
murmota <nega.kuz@gmail.com>
2023-12-26 15:12:10 +0300

    more 2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ mnist = tf.keras.datasets.mnist
 # x_test = tf.keras.utils.normalize(x_test, axis=1)
 #
 # model = tf.keras.models.Sequential([])
-# model.add(tf.keras.layers.Flatten(input   _shape=(28, 28)))
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 # model.add(tf.keras.layers.Dense(128, activation="relu"))
 # model.add(tf.keras.layers.Dense(128, activation="relu"))
 # model.add(tf.keras.layers.Dense(10, activation="softmax"))
